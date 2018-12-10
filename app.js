88e0425b9bd5b8fa47693bf58352d5162013ce00
timmyEvil <timmy@cht.com.tw>
2018-12-10 14:06:21 +0800

    lab2 modify

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 
     app.controller('appController', function ($scope) {
         $scope.title = 'NTA';
-        $scope.subtitle = 'A place to share your knowledge.';
+        $scope.subtitle = 'National Treasury Administration, Ministry of Finance';
     });
 
 })();
