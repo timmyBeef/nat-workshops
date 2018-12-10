8c24e742cd76fa309dfa27982cfd73365206a3db
timmyEvil <timmy@cht.com.tw>
2018-12-10 11:55:21 +0800

    env add controller continue

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
 (function () {
-    var app = angular.module("app", ['ntaCommonModule']);
-
-    app.factory('appService', function (cResource) {
-
-        var resource = cResource('', {});
-
-        return {
-            execute: function (url, param) {
-                var options = { 'omitAlerts': false, 'clearAlerts': true };
-                return resource.execute(url, param, options);
-            }
-        };
-    });
+    var app = angular.module("app", []);
 
     app.controller('appController', function ($scope) {
 
