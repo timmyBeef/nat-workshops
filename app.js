8282a25fabeeea322206be65eb436c0d62396604
timmyEvil <timmy@cht.com.tw>
2018-12-10 13:51:58 +0800

    lab 2

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
     var app = angular.module("app", []);
 
     app.controller('appController', function ($scope) {
-
+        $scope.title = 'NTA';
+        $scope.subtitle = 'A place to share your knowledge.';
     });
 
 })();
